15bcda40b1ab045628b9c904cec6cb385e00ba03
DaniloNicacio <danilonicacio2016@gmail.com>
2024-02-25 19:13:15 -0400

    [FEAT] - Adicionado conteudo da aula 15

---
+++ aula15/index.js
@@ -0,0 +1,13 @@
+let num1 = 9.54578
+let num2 = Math.ceil(num1)
+let num3 = 7.4036
+let num4 = Math.floor(num3)
+let num5 = 3.24896
+let num6 = Math.round(num5)
+let num7 = Math.random()
+console.log(num2)
+console.log(num4)
+console.log(num6)
+console.log(num7)
+console.log(Math.max(2698, 154, 123, 12, 0, 28, -47, 23, -1203, 5, 1000))
+console.log(Math.min(2698, 154, 123, 12, 0, 28, -47, 23, -1203, 5, 1000))
